ad89840ae00b06c3a3bca3442ddd38387703c880
Jonathan Carroll <Jonathan Carroll>
2016-06-11 11:23:10 -0700

    Second Form Created

---
+++ ContactManager/ContactManager/Form2.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form2
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub GroupBox2_Enter(sender As Object, e As EventArgs) Handles GroupBox2.Enter
+
+    End Sub
+End Class
