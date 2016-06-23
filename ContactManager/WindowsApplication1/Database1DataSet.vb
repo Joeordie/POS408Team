3c68ec6f4fc81271f91db1adc49df867497fb349
Jonathan Carroll <carrolljp@joeordie.com>
2016-06-23 14:42:41 -0700

    catchup

---
+++ ContactManager/WindowsApplication1/Database1DataSet.vb
@@ -0,0 +1,10 @@
+﻿Partial Class Database1DataSet
+    Partial Class Table1DataTable
+
+        Private Sub Table1DataTable_Table1RowChanging(sender As Object, e As Table1RowChangeEvent) Handles Me.Table1RowChanging
+
+        End Sub
+
+    End Class
+
+End Class
---
+++ ContactManager/WindowsApplication1/Form1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Data.SqlClient
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        LoadData()
+    End Sub
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub LoadData()
+        Dim connection = Database1DataSet.Table1
+
+    End Sub
+
+End Class
