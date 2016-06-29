617b0b98b0f087187274d6285dbccbfdf30769ff
David <Rippeedj@gmail.com>
2016-06-29 10:31:50 -0700

    added buttons

--- ContactManager/frmMulti.vb
+++ ContactManager/frmMulti.vb
@@ -1,8 +1,6 @@
 ﻿Public Class frmMulti
 
-    Private Sub frmMulti_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
+   
 
     Public Sub clear()
         lstMultipleResults.Items.Clear()
@@ -12,5 +10,26 @@
         lstMultipleResults.Items.Add(strResultLine)
     End Sub
 
+
+   
+    Private Sub btnBack_Click(sender As Object, e As EventArgs) Handles btnMultiBack.Click
+        My.Forms.frmSearch.Show()
+        Me.Close()
+    End Sub
+
+    Private Sub btnSelect_Click(sender As Object, e As EventArgs) Handles btnSelect.Click
+        'lstMultipleResults.SelectedIndex()
+        'Verify that an item was selected
+        If lstMultipleResults.SelectedIndex = -1 Then
+            'If ListIndex is -1, nothing selected
+            MsgBox("Nothing was selected!")
+        Else
+            'If ListIndex not -1 inform user what was selected
+            MsgBox("You selected " & lstMultipleResults.SelectedIndex.ToString)
+        End If
+
+    End Sub
+
+   
 
 End Class
