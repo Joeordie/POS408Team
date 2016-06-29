cdd53a560863b77e0595414595625024c7c2288a
David <Rippeedj@gmail.com>
2016-06-29 12:21:17 -0700

    updated Multiform with substring and FormClosing

--- ContactManager/frmMulti.vb
+++ ContactManager/frmMulti.vb
@@ -25,11 +25,15 @@
             MsgBox("Nothing was selected!")
         Else
             'If ListIndex not -1 inform user what was selected
-            MsgBox("You selected " & lstMultipleResults.SelectedIndex.ToString)
+            MsgBox("You selected " & lstMultipleResults.SelectedItem.ToString.Substring(0, 2))
         End If
 
     End Sub
+    'I had a problem: when closing this form with the "X" the form would close but left the program running with no visible forms
+    Private Sub frmMulti_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'If form is closed using the "X", it will return to search form  
 
-   
+        My.Forms.frmSearch.Show()
 
+    End Sub
 End Class
