56e8f4059180ebce56c716676d7169c1c4f53fcc
Jonathan Carroll <carrolljp@joeordie.com>
2016-06-29 12:46:14 -0700

    added davids closer again

--- ContactManager/frmMulti.vb
+++ ContactManager/frmMulti.vb
@@ -33,7 +33,11 @@
         End If
 
     End Sub
+    'I had a problem: when closing this form with the "X" the form would close but left the program running with no visible forms
+    Private Sub frmMulti_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        'If form is closed using the "X", it will return to search form  
 
+        My.Forms.frmSearch.Show()
 
 
 End Class
